cbdf8d4d324386e999faf210e1fe4662599827c4
anotherbondan <anandagautama3112@gmail.com>
2025-09-03 13:28:15 +0700

    Complete tutorial 1: Django MVT implementation

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406352613',
+        'name': 'Ananda Gautama Sekar Khosmana',
+        'class': 'PBP D'
+    }
+
+    return render(request, "main.html", context)
